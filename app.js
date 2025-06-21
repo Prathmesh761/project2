f86cef66e6fe2dbba9d7093d4bca400db09cadab
Prathmesh <dhoneprathmesh72@gmail.com>
2025-06-21 14:28:45 +0530

    added button

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
 //Adding new Feature
+let btn=document.querySelector("#btn");
+let head=document.querySelector("#heading");
+btn.addEventListener(cancelIdleCallback,()=>{
+    head.style.innerText="Button CLicked";
+})
